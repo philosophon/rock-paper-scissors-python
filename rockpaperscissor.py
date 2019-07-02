5a81daf95d08ca6961b26255beb9eb02fbe97f98
roboneptune15 <52420143+roboneptune15@users.noreply.github.com>
2019-07-02 12:36:28 -0500

    Add files via upload

--- rockpaperscissor.py
+++ rockpaperscissor.py
@@ -1,6 +1,7 @@
 import random
-
-while True:
+player_wins=0
+computer_wins=0
+while player_wins<3:
 	print("make your guess:", end=" ")
 	guess = str(input())
 	guess = guess.lower()
@@ -14,18 +15,26 @@ while True:
 		elif guess == 'rock':
 			if computer_guess == 'scissors':
 				print("you win!")
+				player_wins= player_wins+1
 			elif computer_guess == 'paper':
 				print("you lose!")
+				computer_wins+=1
 		elif guess == 'paper':
 			if computer_guess =='rock':
 				print("you win!")
+				player_wins+=1
 			elif computer_guess == 'scissors':
 				print("you lose!")
+				computer_wins+=1
 		elif guess == 'scissors':
 			if computer_guess == 'paper':
 				print("you win!")
+				player_wins+=1
 			if computer_guess == 'rock':
 				print("you lose!")
+				computer_wins+=1
+	print("you've won ", player_wins)
+
 
 
 
